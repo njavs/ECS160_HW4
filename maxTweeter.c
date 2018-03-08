41b13e2db1b1aab23a452eb81acdfbc2efe32055
[akshephard] <akshephard@ucdavis.edu>
2018-03-08 12:15:39 -0800

    added sort.c

--- maxTweeter.c
+++ maxTweeter.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 #include "csvparser.h"
+#define TWEET_MAX 20000
+struct tweet_container {
+  char tweet_name_array[TWEET_MAX][16];
+};
 
 int main() {
     int i =  0;
+    int tweet_count = 0;
+    struct tweet_container * all_tweeters;
     //                                   file, delimiter, first_line_is_header?
     CsvParser *csvparser = CsvParser_new("cl-tweets-short.csv", ",", 1);
     CsvRow *header;
@@ -15,6 +21,7 @@ int main() {
         printf("%s\n", CsvParser_getErrorMessage(csvparser));
         return 1;
     }
+    all_tweeters = malloc(sizeof(struct tweet_container));
     char **headerFields = CsvParser_getFields(header);
     for (i = 0 ; i < CsvParser_getNumFields(header) ; i++) {
         printf("TITLE: %s\n", headerFields[i]);
@@ -27,8 +34,14 @@ int main() {
         //     printf("FIELD: %s\n", rowFields[i]);
         // }
         printf("FIELD: %s\n", rowFields[8]);
+        strncpy(all_tweeters->tweet_name_array[tweet_count],rowFields[8],16);
+        //all_tweeters->tweet_name_array[tweet_count]
+        printf("tweet container at %d: %s\n",tweet_count, all_tweeters->tweet_name_array[tweet_count] );
+
         CsvParser_destroy_row(row);
+        tweet_count++;
     }
     CsvParser_destroy(csvparser);
+    free(all_tweeters);
     return 0;
 }
